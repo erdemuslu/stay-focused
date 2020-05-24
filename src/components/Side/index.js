28b35e3d8c3c584ddec0d2fcf66caee50752e67a
Erdem Uslu <usluerdem@yandex.com>
2020-05-24 16:47:30 +0300

    update :: progress view

--- src/components/Side/index.js
+++ src/components/Side/index.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 
 import Selection from '../Selection';
-import ProgressBar from '../ProgressBar';
+// import ProgressBar from '../ProgressBar';
 import Timer from '../Timer';
 
 const Side = () => {
   const [start, setStart] = useState(false);
-  const [progressBarValue, setProgressBarValue] = useState(100);
+  // const [progressBarValue, setProgressBarValue] = useState(100);
   const [time] = useState(Date.now() + (60 * 25 * 1000));
 
   const handleTimer = () => {
     setStart((prevState) => !prevState);
   };
 
-  const handleTick = () => {
-    setProgressBarValue((prevState) => prevState - (100 / (60 * 25)));
-  };
+  // const handleTick = () => {
+  //   setProgressBarValue((prevState) => prevState - (100 / (60 * 25)));
+  // };
 
   return (
     <div className="side">
@@ -25,25 +25,25 @@ const Side = () => {
       </div>
       <div className="side-content">
         <div className="side-progress">
-          <ProgressBar
+          {/* <ProgressBar
             value={progressBarValue}
-          />
+          /> */}
           <Timer
             autoStart={false}
             start={start}
             time={time}
-            onTick={handleTick}
+            // onTick={handleTick}
           />
         </div>
-        <div className="side-cta">
-          <button
-            type="button"
-            className="btn"
-            onClick={handleTimer}
-          >
-            {start ? 'Pause' : 'Start'}
-          </button>
-        </div>
+      </div>
+      <div className="side-cta">
+        <button
+          type="button"
+          className="btn"
+          onClick={handleTimer}
+        >
+          {start ? 'Pause' : 'Start'}
+        </button>
       </div>
     </div>
   );
